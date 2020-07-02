7844debca3c203ea7a0c97e1feb5f8c2d46336e3
Nupur Pandey <1996nupurpandey@gmail.com>
2020-07-02 20:01:16 +0530

    Updates

---
+++ precache-manifest.cd8e829be5d93d403ac506b66304f1a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e43fbd760edce3ad7deb0107f62a65d",
+    "url": "/detangleevents/index.html"
+  },
+  {
+    "revision": "f0973b3b61b50c21b478",
+    "url": "/detangleevents/static/css/2.9bfc685a.chunk.css"
+  },
+  {
+    "revision": "f3fb1e4ca35efe826efa",
+    "url": "/detangleevents/static/css/main.f3f32dc1.chunk.css"
+  },
+  {
+    "revision": "f0973b3b61b50c21b478",
+    "url": "/detangleevents/static/js/2.e1fd06ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/detangleevents/static/js/2.e1fd06ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3fb1e4ca35efe826efa",
+    "url": "/detangleevents/static/js/main.1abdf71c.chunk.js"
+  },
+  {
+    "revision": "19c248f48211e99ca29b",
+    "url": "/detangleevents/static/js/runtime-main.e942a418.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/detangleevents/static/media/logo.5d5d9eef.svg"
+  }
+]);
